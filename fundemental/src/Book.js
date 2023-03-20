6dadb5dafc60f35d4418f8065e471d612c015895
obinnafranklinduru <obinnafranklinduru.com>
2023-03-20 11:22:54 +0100

    initial commit

---
+++ fundemental/src/Book.js
@@ -0,0 +1,12 @@
+const Book = (props) => {
+    const { img, title, author, index } = props;
+    return (
+        <article className='book'>
+            <img src={img} alt={title} />
+            <h2>{title}</h2>
+            <h4>{author} </h4>
+            <span className="index">#{index + 1}</span>
+        </article>
+    );
+};
+export default Book;
---
+++ fundemental/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './style.css';
+
+import { books } from './books';
+import Book from './Book';
+
+function BookList() {
+  return (
+    <>
+      <h1>amazon best sellers</h1>
+      <section className='booklist'>
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} index={index} />;
+        })}
+      </section>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList />);
